d740239c367863e54ab66c84cc3d600b85df2821
Brett Morris <brettmorris21@gmail.com>
2017-11-08 14:58:03 -0800

    simplifying test matrix

--- mrspoc/tests/test_centroid.py
+++ mrspoc/tests/test_centroid.py
@@ -1,15 +1,17 @@
 import numpy as np
+import pytest
 
 from ..star import Star, Spot
 
 
-def test_col_methods():
+@pytest.mark.parametrize('x', (0.8, 0.6, 0.4, 0.2, 0.0))
+def test_col_methods(x):
     """
     python -c "from mrspoc.tests.test import test_col_methods as f; f()"
     """
 
     star = Star()
-    star.spots = [Spot(x=0.8, y=0.00, r=0.2)]
+    star.spots = [Spot(x=x, y=0.00, r=0.2)]
 
     x0, y0 = star._centroid_analytic()
     x1, y1 = star._centroid_numerical()
