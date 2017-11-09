0b457b0343a1742bae211471562dd891bdfd8e71
Brett Morris <brettmorris21@gmail.com>
2017-11-08 16:25:50 -0800

    Updating test

--- mrspoc/tests/test_centroid.py
+++ mrspoc/tests/test_centroid.py
@@ -5,16 +5,33 @@ from ..star import Star, Spot
 
 
 @pytest.mark.parametrize('x', (0.8, 0.6, 0.4, 0.2, 0.0))
-def test_col_methods(x):
+def test_centroid_small_spots(x):
     """
-    python -c "from mrspoc.tests.test import test_col_methods as f; f()"
+    python -c "from mrspoc.tests.test import test_centroid_small_spots as f; f()"
     """
-
     star = Star()
-    star.spots = [Spot(x=x, y=0.00, r=0.2)]
+    star.spots = [Spot(x=x, y=0.0, r=0.01)]
 
     x0, y0 = star._centroid_analytic()
     x1, y1 = star._centroid_numerical()
 
     diff = np.sqrt((x0 - x1)**2 + (y0 - y1)**2)
-    assert diff < star.r / 100
+    assert diff < 0.00001
+
+
+@pytest.mark.parametrize('x', (0.8, 0.6, 0.4, 0.2, 0.0))
+def test_centroid_large_spots(x):
+    """
+    python -c "from mrspoc.tests.test import test_centroid_large_spots as f; f()"
+    """
+    star = Star()
+    star.spots = [Spot(x=x, y=0.0, r=0.1)]
+
+    x0, y0 = star._centroid_analytic()
+    x1, y1 = star._centroid_numerical()
+
+    diff = np.sqrt((x0 - x1)**2 + (y0 - y1)**2)
+
+    # Allow for bigger error because large spots are more affected by difference
+    # in limb-darkening across the spot.
+    assert diff < 0.0005
