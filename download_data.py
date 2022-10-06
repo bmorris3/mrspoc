ecd3a051e320cb1fcce5c07b75df5bf469d68087
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-06 04:38:18 +0000

    Adding tarfile member sanitization to extractall()

--- download_data.py
+++ download_data.py
@@ -16,7 +16,26 @@ urlretrieve(data_archive_url, data_archive_path)
 
 # Extract all files from the gzipped tar archive
 with tarfile.open(data_archive_path, 'r|gz') as f:
-    f.extractall()
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(f)
 
 # Remind the user to set an environment variable:
 print('Now set an environment variable "MRSPOC_DATA_DIR" to the path to the '
